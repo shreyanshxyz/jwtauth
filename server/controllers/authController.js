25520952ad4d03c6c788fa917d02023f331ea5bb
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 23:51:16 +0530

    update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,45 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import User from "../models/userModel.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const signUp = () => {};
-export const login = () => {};
+async function signUp(req, res) {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res
+      .status(201)
+      .json(
+        `User successfully saved. username: ${username} password: ${hashedPassword}`
+      );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function login(req, res) {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    res.json({ message: "Login successful!" });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export { signUp, login };
