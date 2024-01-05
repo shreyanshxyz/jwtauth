e469f04456b2a781fe5b7a46bf6be6361920e4a7
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 20:50:48 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import dbConnect from "./util/dbConnect.js";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/", (req, res) => {
   res.send("<h1>App is running successfully</h1>");
 });
+app.use("/", authRoute);
 
 app.listen(PORT, function () {
   console.log(`App is running on the Port: ${PORT}`);
