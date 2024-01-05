87bed285a8c6037d22ac5df6af8c3f9352730b89
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 20:41:36 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ dbConnect();
 const PORT = 5000 || process.env.PORT;
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.send("<h1>App is running successfully</h1>");
+});
+
 app.listen(PORT, function () {
   console.log(`App is running on the Port: ${PORT}`);
 });
