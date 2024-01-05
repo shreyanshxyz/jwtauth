43e39c7e67c8f2fdf6024c8dc9023ac48da397a6
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 20:47:35 +0530

    update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signUp = () => {};
 export const login = () => {};
