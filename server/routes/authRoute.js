ed7fd1ea229f994c2be12c0aab07db034679f2f1
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 20:50:52 +0530

    update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,1 +1,9 @@
-import express from express
+import express from "express";
+import { signUp, login } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/api/singup", signUp);
+router.post("/api/login`", login);
+
+export default router;
