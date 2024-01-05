1df8bb2a6691b2a7ff1d203704e69579e3a350e6
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-06 00:04:34 +0530

    update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
@@ -46,4 +45,44 @@ async function login(req, res) {
   }
 }
 
-export { signUp, login };
+async function editUser(req, res) {
+  try {
+    const { userId } = req.user;
+
+    // Find the user by ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Update the user's username
+    user.username = req.body.newUsername;
+    await user.save();
+
+    res.json({ message: "Username updated successfully!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+async function deleteUser(req, res) {
+  try {
+    // Access the decoded user information from the token
+    const { userId } = req.user;
+
+    // Find and delete the user by ID
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export { signUp, login, editUser, deleteUser };
