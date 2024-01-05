0ae40a95d994dd2e272073fed9d581547f1c324a
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-05 23:56:00 +0530

    update verifyToken.js

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -2,4 +2,16 @@ import jwt from "jsonwebtoken";
 
 function verifyToken(req, res, next) {
   const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized - No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
 }
