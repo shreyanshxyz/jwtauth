cc21b6cacdf8a314f772263c2e7f38c8df428820
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 18:35:02 +0530

    update EditUsername.jsx

--- client/src/components/EditUsername.jsx
+++ client/src/components/EditUsername.jsx
@@ -2,15 +2,6 @@ function EditUsername() {
   return (
     <div>
       <h1>Edit Username</h1>
-      <form>
-        <div className="input-container">
-          <label>New </label>
-          <input type="text" name="newUsername" required />
-        </div>
-        <div className="button-container">
-          <input type="submit" />
-        </div>
-      </form>
     </div>
   );
 }
