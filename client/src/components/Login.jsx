896548a441d3746c718969e41e5713bb6527cc5e
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 11:37:30 +0530

    update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(username, password);
   return (
     <div className="form">
       <h1>Login</h1>
       <form>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="uname" required />
+          <input
+            type="text"
+            name="uname"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input
+            type="password"
+            name="pass"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="button-container">
           <input type="submit" />
         </div>
       </form>
+      <Link to="/signup">
+        <button>Sign Up</button>
+      </Link>
     </div>
   );
 }
