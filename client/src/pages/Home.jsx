7269f2f432ca5924769598c9f68104e73876f6ab
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 11:27:41 +0530

    update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,11 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <h4>Username</h4>
+      <button>Logout</button>
+    </div>
+  );
 }
 
 export default Home;
