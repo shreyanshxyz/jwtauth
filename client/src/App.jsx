c29956b291f6e981c97cf24da95d33d96d00e020
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 18:34:56 +0530

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import "./App.css";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
+  const user = localStorage.getItem("token");
+  console.log(user);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/signup"
+          element={user ? <Navigate to="/" /> : <Signup />}
+        />
       </Routes>
     </BrowserRouter>
   );
