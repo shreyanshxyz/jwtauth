a8e1cbf038f67717f8772990ed4826396767e3c9
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 11:37:28 +0530

    update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(username, password);
+
   return (
     <div className="form">
       <h1>Sign Up</h1>
       <form>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="uname" required />
+          <input
+            type="text"
+            name="uname"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input
+            type="password"
+            name="pass"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="button-container">
           <input type="submit" />
         </div>
       </form>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
