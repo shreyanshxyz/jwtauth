323cad6dcc2a474420041f800d3a5e07fefe8c62
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-08 18:35:11 +0530

    update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+  function handleLogout() {
+    localStorage.clear();
+    window.location.href = "/login";
+    navigate("/login");
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
       <h4>Username</h4>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
